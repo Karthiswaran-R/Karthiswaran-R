02f5c99805589fc24d8e3b3edc02a5e0ea9d5a97
Karthiswaran-R <karthi28974@gmail.com>
2025-01-24 00:25:39 +0530

    Create header.v

---
+++ header.v
@@ -0,0 +1,24 @@
+// =======================================================
+// Company: VLSI Design Hub
+// File: header.vh
+// Description: Global header file for Verilog projects
+// Author: VLSI Design Hub Team
+// Date: January 2025
+// =======================================================
+
+// Version Information
+`define VERSION "1.0.0"
+
+// Company Information
+`define COMPANY_NAME "VLSI Design Hub"
+`define CONTACT_EMAIL "support@vlsidesignhub.com"
+
+// Debugging Macros
+`define DEBUG_MODE   // Uncomment to enable debug mode
+
+// Logic Values
+`define HIGH 1'b1      // Logic high
+`define LOW  1'b0      // Logic low
+
+// End of header file
+// =======================================================
